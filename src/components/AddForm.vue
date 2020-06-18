4f677fcf622e9f6f67e56e099651d4c80f01dfce
anthonyophernandez <anthonyophernandez@gmail.com>
2020-06-18 10:00:45 +0200

    Add Task Form Component

---
+++ src/components/AddForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form class="flex flex-col w-full m-auto">
+
+    <div>
+      <label class="block text-gray-700 text-lg font-bold" for="description">
+        Task:
+      </label>
+      <textarea class="w-full h-24 p-2 bg-gray-900 rounded text-white resize-none" id="description" type="text" placeholder="Task Description" />
+    </div>
+
+    <div class="flex flex-col my-2">
+      <label class="block text-gray-700 text-lg font-bold" for="impact">
+        Impact: {{ impact }}
+      </label>
+      <input type="range" min="1" max="10" value="5" id="impact" v-model="impact">
+      <label class="block text-gray-700 text-lg font-bold" for="confidence">
+        Confidence: {{ confidence }}
+      </label>
+      <input type="range" min="1" max="10" value="5" id="confidence" v-model="confidence">
+      <label class="block text-gray-700 text-lg font-bold" for="ease">
+        Ease: {{ ease }}
+      </label>
+      <input type="range" min="1" max="10" value="5" id="ease" v-model="ease">
+      <p class="text-gray-700 text-lg font-bold">ICE Score: {{ iceScore }}</p>
+    </div>
+
+    <div>
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded" type="button">Add</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddForm',
+  data () {
+    return {
+      impact: 5,
+      confidence: 5,
+      ease: 5
+    }
+  },
+  computed: {
+    iceScore () {
+      const result = (parseInt(this.impact) + parseInt(this.confidence) + parseInt(this.ease)) / 3
+      return result.toFixed(2)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="bg-gray-900 p-4 w-full xl:w-4/5 flex flex-wrap">
+    <div class="w-full bg-white border rounded-lg overflow-hidden p-6 my-2 mx-2">
+      <AddForm />
+    </div>
     <div class="xl:w-1/4 lg:w-1/3 md:w-1/2 sm:w-full" v-for="task in tasks" :key="task.id">
       <Card :task="task" class="my-2 mx-2"/>
     </div>
@@ -9,11 +12,13 @@
 <script>
 import Api from '../services/api'
 import Card from '../components/Card.vue'
+import AddForm from '../components/AddForm.vue'
 
 export default {
   name: 'Home',
   components: {
-    Card
+    Card,
+    AddForm
   },
   data () {
     return {
