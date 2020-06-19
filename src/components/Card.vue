f1a68f70b9db5e8cca0859166ef7db5d6527e30e
anthonyophernandez <anthonyophernandez@gmail.com>
2020-06-19 09:50:20 +0200

    Show/Hide Description, ICE Score Color

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,11 @@
 <template>
 
-  <div class="bg-white border rounded-lg overflow-hidden p-6">
+  <div class="bg-white border rounded-lg p-6">
 
     <h4 class="mb-2 text-gray-500 text-xl text-center uppercase font-extrabold tracking-widest">Task {{ task.id }}</h4>
     <div class="flex mb-2">
       <div class="w-2/3 mr-4">
-        <div class="bg-gray-500 rounded-full m-auto">
+        <div class="bg-gray-500 rounded-full m-auto" :class="color">
           <p class="text-center text-6xl font-bold">{{ task.iceScore }}</p>
         </div>
       </div>
@@ -19,8 +19,9 @@
     <hr class="mx-1 border-1 border-gray-900">
 
     <div class="p-1">
-      <h5 class="mb-1 text-gray-500 text-xs font-semibold uppercase tracking-wide">Description:</h5>
-      <p>{{ task.description }}</p>
+      <h5 class="mb-1 text-gray-500 text-xs font-semibold uppercase tracking-wide">Description <span class="cursor-pointer text-lg text-right text-gray-900" @click="showDescription = !showDescription" v-html="symbol"></span></h5>
+      <p v-if="showDescription">{{ task.description }}</p>
+      <p v-else class="truncate">{{ task.description }}</p>
     </div>
 
   </div>
@@ -30,7 +31,32 @@
 <script>
 export default {
   name: 'Card',
-  props: ['task']
+  props: ['task'],
+  data () {
+    return {
+      showDescription: false
+    }
+  },
+  computed: {
+    symbol () {
+      return this.showDescription ? '&#45;' : '&#43;'
+    },
+    color () {
+      let rankColor = ''
+      if (this.task.iceScore < 3) {
+        rankColor = 'bg-red-600'
+      } else if (this.task.iceScore < 5) {
+        rankColor = 'bg-orange-600'
+      } else if (this.task.iceScore < 8) {
+        rankColor = 'bg-green-500'
+      } else if (this.task.iceScore < 10) {
+        rankColor = 'bg-green-700'
+      } else {
+        rankColor = 'bg-yellow-500'
+      }
+      return rankColor
+    }
+  }
 }
 </script>
 
